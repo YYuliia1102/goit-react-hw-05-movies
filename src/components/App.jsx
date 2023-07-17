bf041ebf877dae377fdce63093272fd4fe1d30c1
YYuliia1102 <reya1102@gmail.com>
2023-07-17 15:21:21 +0200

    task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,32 @@
+import React, { Suspense } from 'react';
 import { NavLink, Route, Routes } from 'react-router-dom';
-import HomePage from 'pages/HomePage';
-import Movies from 'pages/Movies';
-import MovieDetails from 'pages/MovieDetails';
 import styles from './App.module.css';
+
+const HomePage = React.lazy(() => import('pages/HomePage'));
+const Movies = React.lazy(() => import('pages/Movies'));
+const MovieDetails = React.lazy(() => import('pages/MovieDetails'));
 
 export const App = () => {
   return (
-    <div className={styles.app}>
+    <div className={styles.container}>
       <header className={styles.header}>
         <nav>
-          <NavLink to="/" className={styles.navLink} activeClassName={styles.activeNavLink}>
+          <NavLink to="/" className={styles.navLink}>
             Home
           </NavLink>
-          <NavLink to="/movies" className={styles.navLink} activeClassName={styles.activeNavLink}>
+          <NavLink to="/movies" className={styles.navLink}>
             Movies
           </NavLink>
         </nav>
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetails />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
